76703a34f8f81f2076eb72160567ceae1b77f49a
alfre <alfreagor@gmail.com>
2023-01-06 00:53:17 -0300

    filtrosydetails

---
+++ tarea1/assets/detail.js
@@ -0,0 +1,61 @@
+let cadenaParametroUrl = location.search;// para tomar la dire de las tarjetas
+let parametros = new URLSearchParams(cadenaParametroUrl); // para vincular la direcion que teniamos en las tarjetas x medio del get
+let idCard = parametros.get("idUrl");//nombre del id que usamos para vincular cada tarjeta 
+
+let contenedor = document.getElementById("detailed-card");//capturamos un elemento donde trabajaremos en el
+
+let propiedadEvents = data.events;
+
+let cardEncontrada = propiedadEvents.find(
+  (propiedadEvents) => propiedadEvents._id == idCard
+);
+
+function pintarCard(propiedadEvents) {
+  contenedor.innerHTML = "";
+  let asistencia = "";
+  if(propiedadEvents.assistance){
+  asistencia= `
+    <dt>Assistance:</dt>
+                            <dd>${propiedadEvents.assistance}</dd>`
+  } else {
+    asistencia= `
+    <dt>Estimate:</dt>
+                            <dd>${propiedadEvents.estimate}</dd>`
+  }
+  let template = `
+    <img  class="detailed-img" src="${propiedadEvents.image}" alt="${propiedadEvents.name}">
+ 
+                    <section class="detailed-descrip p-4">
+                        <h2 class="text-center">${propiedadEvents.name}</h2>
+                        <dl>
+                            <dt>Date:</dt>
+                            <dd>${propiedadEvents.date}</dd>
+
+                            <dt>Description:</dt>
+                            <dd>${propiedadEvents.description}</dd>
+                            <dt>Category:</dt>
+                            <dd>${propiedadEvents.category}</dd>
+                            <dt>Place:</dt>
+                            <dd>${propiedadEvents.place}</dd>
+                            <div class="d-flex  justify-content-between flex-wrap">
+                            <div>
+                            <dt>Capacity:</dt>
+                            <dd>${propiedadEvents.capacity}</dd>
+                            </div>
+                            <div>
+                            ${asistencia}
+                            </div>
+                            <div>
+                            <dt>Price:</dt>
+                            <dd>${propiedadEvents.price}</dd>
+                            </div>
+                            </div>
+                        </dl>
+                    </section>`;
+
+  contenedor.innerHTML = template;
+}
+
+pintarCard(cardEncontrada);
+
+
--- tarea1/assets/index.js
+++ tarea1/assets/index.js
@@ -1,36 +1,96 @@
-let espaciotrabajo = document.getElementById("tarjetas")
-console.log(espaciotrabajo)
 
+let indexjs = document.getElementById("cards-section")
+const search = document.getElementById('search1')
+const check = document.getElementById("checkbox3")
 
-let tarjeta_eventos = document.createElement("div")
-
-let eventos = data.events
-
-
-console.log(eventos)
-
-for (let index = 0; index < eventos.length; index++) {
-  const element = eventos[index];
-
-  
-  tarjeta_eventos.innerHTML+=`<div class="card" style="width: 20rem;">
-  <img src=${element.image} class="card-img-top imatarj" alt=${element.name}>
-  <div class="card-body">
-    <h5 class="card-title"> ${element.name} </h5>
-    <p class="card-text"> ${element.description} </p>
-    <div class="finalcarta">
-      <p>Price $ ${element.price} </p>
-      <a href="./details.html" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  </div>`
+function craftCards(lista, descriptionCards){
+    let imagenes = ""
+    for (let propiedadEvents of lista){
+            let template =  `
+                <div class="card" style="width: 16rem;">
+                <img src="${propiedadEvents.image}" class="card-img-top" alt="${propiedadEvents.name}">
+                    <div class="card-body">
+                    <h5 class="card-title">${propiedadEvents.name}</h5>
+                    <p class="card-text">${propiedadEvents.date}</p>
+                    <p class="card-text">${propiedadEvents.category}</p>
+                    <p class="card-text">${propiedadEvents.place}</p>
+                    <p class="card-text">${propiedadEvents.price}</p>
+                    <a href="./details.html?idUrl=${propiedadEvents._id}" class="btn btn-primary">View More</a>
+                </div>
+                </div> `
+    imagenes =  imagenes + template
+    }
+    return imagenes
 }
 
-tarjeta_eventos.className="tarjetero"
+renderTemplate (craftCards(data.events), indexjs)
 
+//Funcion para filtrar categorias
 
+const sinRepetir = []
+const categorias = data.events.map(events => events.category)
 
+categorias.forEach(categorias => {
+    if (!sinRepetir.includes (categorias)){
+        sinRepetir.push (categorias)}
+    })
+    
+//Creacion de los botones checkbox
+    function generarCheckbox (categorias){
+        let template = ""
+        categorias.forEach(categoria =>{
+            template += `<div class="form-check d-flex">   
+            <label class="form-check-label">${categoria}
+            <input class="form-check-input" type="checkbox" value="${categoria}">
+            </label>
+            </div>`
+        })
+        return template
+    }
+    check.innerHTML = generarCheckbox(sinRepetir)
+    //inner para pasar checks a pantaia
+    let checkbuttons = document.querySelectorAll(".form-check-input")
+    //funcion para el filtro de los check
+    function checkFilter (touchs, categoriesList){
+        let values = [];
+        for (let touch of touchs){
+            if (touch.checked)
+            values.push(touch.value.toLowerCase())
+        }
+        let filters = categoriesList.filter(food => values.includes(food.category.toLowerCase()))
+        if (values.length === 0){
+            return categoriesList
+        }
+        else{
+            return filters
+        }
+    }
+    check.addEventListener('change', filtroCruzado)
+    
+//funcion para el filtro del search
+search.addEventListener( 'input', filtroCruzado)
 
+function searchFood(inputFind, categoriesList){
+    const filterFood = categoriesList.filter(food => {
+        return food.name.toLowerCase().startsWith(inputFind.value.toLowerCase())
+    });
+    return filterFood
+}
+// funcion del filtro cruzado
+function filtroCruzado(evento){
+    const filterPerFind = searchFood (search, data.events)
+    const filterPerCheack = checkFilter (checkbuttons, filterPerFind)
+    if(filterPerCheack.length === 0) {
+        let alert = `<h3 class="alert">THERES NO COICIDENCES WITH YOUR SEARCH</h3>`
+        renderTemplate(alert, indexjs)
+    }
+    else {
+        renderTemplate(craftCards(filterPerCheack), indexjs)
+    }
+}
+//funcion del rendertemplate
+function renderTemplate(template, ubicacion){
+    ubicacion.innerHTML = template
+}
 
-espaciotrabajo.appendChild(tarjeta_eventos)
-
+filtroCruzado()
